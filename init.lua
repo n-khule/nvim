b588c7aeff8f8b8625a70811c8de71b5f97fea23
Nikhil Khule <nikhil.akhule+github@gmail.com>
2025-11-30 02:50:01 +0530

    Add .vimrc configurations to neovim

--- init.lua
+++ init.lua
@@ -1,2 +1,18 @@
 require("base.keymaps")
 require("base.lazy")
+
+vim.opt.number = true
+vim.opt.showcmd = true
+
+-- Search behavior
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Indentation
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- Show cursor position
+vim.opt.ruler = true
